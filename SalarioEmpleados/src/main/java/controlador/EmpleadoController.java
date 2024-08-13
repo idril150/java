7b1688a2ed2c1949baa6f4a5859ba601de7d44d3
idril150 <ebibari1@gmail.com>
2024-08-13 16:58:03 -0600

    inicio salario empleados

---
+++ SalarioEmpleados/src/main/java/controlador/EmpleadoController.java
@@ -0,0 +1,15 @@
+package controlador;
+
+import modelo.Empleado;
+import persistencia.EmpleadoJpaController;
+import vista.VistaEmpleado;
+
+public class EmpleadoController {
+    private VistaEmpleado vewEmp;
+    private Empleado emp;
+    private EmpleadoJpaController empCtrl;
+    
+    public EmpleadoController(VistaEmpleado view, Empleado emp){
+        
+    }
+}
---
+++ SalarioEmpleados/src/main/java/salarioempleados/SalarioEmpleados.java
@@ -0,0 +1,16 @@
+package salarioempleados;
+
+import vista.VistaEmpleado;
+
+
+
+public class SalarioEmpleados {
+
+    public static void main(String[] args) {
+        
+        VistaEmpleado view = new VistaEmpleado();
+        view.setVisible(true);
+        view.setLocationRelativeTo(null);
+        
+    }
+}
